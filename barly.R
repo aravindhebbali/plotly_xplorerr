3949a95a54cbdf497ea69f4ba838739863c1a550
rsquaredin <rsquaredin@gmail.com>
2017-06-09 16:00:06 +0530

    bar plots

--- barly.R
+++ barly.R
@@ -12,4 +12,76 @@ p <- plot_ly(data,
 p
 
 # grouped bar chart
-data <- as.data.frame(table(mtcars$am, mtcars$vs))
+Animals <- c("giraffes", "orangutans", "monkeys")
+SF_Zoo <- c(20, 14, 23)
+LA_Zoo <- c(12, 18, 29)
+data <- data.frame(Animals, SF_Zoo, LA_Zoo)
+
+p <- plot_ly(data, x = ~Animals, y = ~SF_Zoo, 
+             type = "bar", name = "SF_Zoo") %>%
+  add_trace(y = ~LA_Zoo, name = "LA_Zoo") %>%
+  layout(yaxis = list(
+    title = 'Count'
+    ),
+    barmode = 'group'
+  )
+
+p <- plot_ly(data, x = ~Animals, y = ~SF_Zoo, 
+             type = "bar", name = "SF_Zoo") %>%
+  add_trace(y = ~LA_Zoo, name = "LA_Zoo") %>%
+  layout(yaxis = list(
+    title = 'Count'
+  ),
+  barmode = 'stack'
+  )
+
+
+# hover text and bar color
+x <- c('Product A', 'Product B', 'Product C')
+y <- c(20, 14, 23)
+text <- c('27% market share', '24% market share', '19% market share')
+data <- data.frame(x, y, text)
+
+p <- plot_ly(data, x = ~x, y = ~y, type = "bar", text = text,
+             marker = list(color = 'blue',
+                           line = list(
+                             color = 'black',
+                             width = 1.5
+                           ))) %>%
+  layout(
+    title = 'Bar Chart',
+    xaxis = list(title = ""),
+    yaxis = list(title = "")
+  )
+
+
+# add labels
+p <- plot_ly(data, x = ~x, y = ~y, type = "bar", text = text,
+             marker = list(color = 'blue',
+                           line = list(
+                             color = 'black',
+                             width = 1.5
+                           ))) %>%
+  layout(
+    title = 'Bar Chart',
+    xaxis = list(title = ""),
+    yaxis = list(title = ""),
+    annotations = list(x = x, y = y, text = y,
+                       xanchor = 'center', yanchor = 'bottom',
+                       showarrow = FALSE)
+  )
+
+
+# bar color: color
+# bar width: width
+# bar color based on a variable
+# bargap
+# bargroupgap
+# barmode = 'relative'
+# horizontal bar chart
+p <- plot_ly(x = c(20, 14, 23), 
+             y = c('giraffes', 'orangutans', 'monkeys'), 
+             type = 'bar', 
+             orientation = 'h')
+
+
