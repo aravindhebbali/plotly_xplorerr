966e702ccfbb38242a7fd098314b7e73632277b6
rsquaredin <rsquaredin@gmail.com>
2017-06-15 22:46:11 +0530

    histogram UDF

--- histly.R
+++ histly.R
@@ -1,32 +1,67 @@
 library(plotly)
 
-# 'type', 'visible', 'showlegend', 'legendgroup', 'opacity', 'name', 
-# 'uid', 'hoverinfo', 'stream', 'x', 'y', 'text', 'orientation', 
-# 'histfunc', 'histnorm', 'autobinx', 'nbinsx', 'xbins', 'autobiny', 
-# 'nbinsy', 'ybins', 'marker', 'error_y', 'error_x', '_deprecated', 
-# 'xaxis', 'yaxis', 'xsrc', 'ysrc', 'textsrc', 'key'
+histly <- function(data = NULL, y = NULL, hist_orient = 'v', 
+                   hist_opacity = 1, hist_type = 'count', auto_binx = TRUE,
+                   xbins_start = NULL, xbins_end = NULL, xbins_size = NULL,
+                   hist_col = 'blue', hist_l_col = 'black', hist_l_w = 1,
+                   hist_bargap = 0) {
+  
+  y1 <- data %>%
+    select_(y) %>%
+    unlist()
+  
+  if(hist_orient == 'v') {
+  
+    p <- plot_ly(data, x = y1, 
+                 type = "histogram", 
+                 opacity = hist_opacity,
+                 histnorm = hist_type,
+                 autobinx = auto_binx,
+                 xbins = list(
+                   start = xbins_start,
+                   end = xbins_end,
+                   size = xbins_size
+                 ),
+                 marker = list(
+                   color = hist_col,
+                   line = list(
+                     color = hist_l_col,
+                     width = hist_l_w
+                   )
+                 )
+    )
+  
+  } else {
+    
+    p <- plot_ly(data, y = y1, 
+                 type = "histogram", 
+                 opacity = hist_opacity,
+                 histnorm = hist_type,
+                 autobinx = auto_binx,
+                 xbins = list(
+                   start = xbins_start,
+                   end = xbins_end,
+                   size = xbins_size
+                 ),
+                 marker = list(
+                   color = hist_col,
+                   line = list(
+                     color = hist_l_col,
+                     width = hist_l_w
+                   )
+                 )
+    )
+    
+  }
+  
+  p <- p %>%
+    layout(
+      bargap = hist_bargap
+    )
+  
+  p
+  
+}
 
-p <- plot_ly(mtcars, y = ~mpg, 
-             type = "histogram", 
-             orientation = "h",
-             opacity = 0.3,
-             histnorm = "probability",
-             autobinx = FALSE,
-             xbins = list(
-               start = 10,
-               end = 34,
-               size = 5
-             ),
-             marker = list(
-               color = "red",
-               line = list(
-                 color = "black",
-                 width = 1
-               )
-             )
-) %>%
-  layout(
-    bargap = 0.1
-  )
-
-p
+h <- histly(mtcars, 'mpg', hist_orient = "h")
+h
